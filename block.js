6945eea95c23a864b97d7550e8b98d44dc8db88e
David <sipos.david.daniel@gmail.com>
2020-09-25 11:34:36 +0200

    genereating key for blocks

--- block.js
+++ block.js
@@ -7,6 +7,14 @@ class Block{
         this.nonce = 0
         this.transactions = []
     }
+
+    get key() {
+        return JSON.stringify(this.transactions) + this.index + this.previousHash + this.nonce
+    }
+
+    addTransaction(transaction){
+        this.transactions.push(transaction)
+    }
 }
 
 module.exports = Block
