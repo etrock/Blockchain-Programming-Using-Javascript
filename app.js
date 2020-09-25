6d10078198cc9fe6cf9ecd9554f2f2c42c0367c1
David <sipos.david.daniel@gmail.com>
2020-09-25 13:51:16 +0200

    setting up express

--- app.js
+++ app.js
@@ -1,3 +1,36 @@
+const Block = require('./block')
+const Blockchain = require('./blockchain')
+const Transaction = require('./transaction')
+
+const express = require('express')
+const app = express()
+
+app.get('/', (req, res) => {
+    res.send("Hello world")
+})
+
+app.get('/blockchain', (req, res) => {
+    let transaction = new Transaction('Mary', 'Jerry', 100)
+    let genesisBlock = new Block()
+    let blockchain = new Blockchain(genesisBlock)
+    let block = blockchain.getNextBlock([transaction])
+    blockchain.addBlock(block)
+
+    let anotherTransaction = new Transaction('Bob', 'James', 300)
+    let anotherBlock = blockchain.getNextBlock([anotherTransaction])
+    blockchain.addBlock(anotherBlock)   
+
+    res.json(blockchain)
+})
+
+
+const port = process.env.port || 3000;
+const server = app.listen(port, () => {
+  console.log(`Listening at http://localhost:${port}/`);
+});
+server.on('error', console.error);
+
+/*
 let Block = require('./block')
 let Blockchain = require('./blockchain')
 let Transaction = require('./transaction')
@@ -17,3 +50,5 @@ blockchain.addBlock(anotherBlock)
 
 
 console.log(blockchain)
+
+*/
