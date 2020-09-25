9369992048d329bbfbd51e9199c24329af373692
David <sipos.david.daniel@gmail.com>
2020-09-25 12:09:20 +0200

    proof of work

--- app.js
+++ app.js
@@ -8,4 +8,12 @@ let genesisBlock = new Block()
 
 let blockchain = new Blockchain(genesisBlock)
 
+let block = blockchain.getNextBlock([transaction])
+blockchain.addBlock(block)
+
+let anotherTransaction = new Transaction('Bob', 'James', 300)
+let anotherBlock = blockchain.getNextBlock([anotherTransaction])
+blockchain.addBlock(anotherBlock)
+
+
 console.log(blockchain)
