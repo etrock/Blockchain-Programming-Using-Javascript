11fa36f090b61c9f52ac1e40036f30f9d27066fe
David <sipos.david.daniel@gmail.com>
2020-09-25 14:22:09 +0200

    adding transactions

--- app.js
+++ app.js
@@ -2,11 +2,24 @@ const Block = require('./block')
 const Blockchain = require('./blockchain')
 const Transaction = require('./transaction')
 
+const bodyParser = require('body-parser')
 const express = require('express')
 const app = express()
 
+
+const transactions = [] 
+
+app.use(bodyParser.json())
+
 app.get('/', (req, res) => {
     res.send("Hello world")
+})
+
+app.post('/transactions', (req, res) => {
+    let {to, from, amount} = req.body
+    const transaction = new Transaction(from,to, amount)
+    transactions.push(transaction)
+    res.json(transactions)
 })
 
 app.get('/blockchain', (req, res) => {
@@ -29,26 +42,3 @@ const server = app.listen(port, () => {
   console.log(`Listening at http://localhost:${port}/`);
 });
 server.on('error', console.error);
-
-/*
-let Block = require('./block')
-let Blockchain = require('./blockchain')
-let Transaction = require('./transaction')
-
-let transaction = new Transaction('Mary', 'Jerry', 100)
-
-let genesisBlock = new Block()
-
-let blockchain = new Blockchain(genesisBlock)
-
-let block = blockchain.getNextBlock([transaction])
-blockchain.addBlock(block)
-
-let anotherTransaction = new Transaction('Bob', 'James', 300)
-let anotherBlock = blockchain.getNextBlock([anotherTransaction])
-blockchain.addBlock(anotherBlock)
-
-
-console.log(blockchain)
-
-*/
